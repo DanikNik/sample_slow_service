78efb454b3b816ca026c38835aaf86537202949b
DanikNik <nikolsky.dan@gmail.com>
2019-10-29 17:26:05 +0300

    ansible

--- ansible_files/roles/deploy/files/aws_populate.sh
+++ ansible_files/roles/deploy/files/aws_populate.sh
@@ -1,15 +1,9 @@
 #!/usr/bin/env bash
 
-echo export PROMETHEUS_ADDR=3.19.211.176 >> .bashrc
-
 sudo apt update
-sudo apt install git -y
-git clone https://github.com/DanikNik/sample_slow_service
-
 sudo apt install apt-transport-https ca-certificates curl software-properties-common -y
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable"
 sudo apt update
 sudo apt install docker-ce -y
 sudo usermod -aG docker ${USER}
-exit
